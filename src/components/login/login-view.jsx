48ea664f6db6be6abba37f6c371481cb91ffa7f2
ChristopherTonn <christopher.tonn@gmail.com>
2022-05-18 17:42:01 +0200

    • ADD login view

--- src/components/login/login-view.jsx
+++ src/components/login/login-view.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react';
+import './login-view.scss';
 
-export class LoginView extends React.Component {
-  render(){
-    return <div> Hello World </div> 
-  }
+export function LoginView(props) {
+  const [ username, setUsername ] = useState('');
+  const [ password, setPassword ] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(username, password);
+    /* Send a request to the server for authentication */
+    /* then call props.onLoggedIn(username) */
+    // props.onLoggedIn(username);
+  };
+
+  return (
+    <form>
+      <label>
+        Username:
+        <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
+      </label>
+      <label>
+        Password:
+        <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+      </label>
+      <button type="submit" onClick={handleSubmit}>Submit</button>
+    </form>
+  );
 }
