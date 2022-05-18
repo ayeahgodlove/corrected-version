4b5fcf55e3bee87fab2b379bf65ea01474b2d96e
ChristopherTonn <christopher.tonn@gmail.com>
2022-05-18 17:43:33 +0200

    • UPDATE main view: - add rest api call, - check if is logged in.

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { SongCard } from "../song-card/song-card";
 import { SongView } from "../song-view/song-view";
+import { LoginView } from '../login/login-view';
+import axios from 'axios';
 
 export class MainView extends React.Component {
 
@@ -13,13 +15,34 @@ export class MainView extends React.Component {
             { _id: 2, Title: 'The Shawshank Redemption', Description: 'desc2...', ImagePath: '...'},
             { _id: 3, Title: 'Gladiator', Description: 'desc3...', ImagePath: '...'}
           ],
-          selectedSong: null
+          selectedSong: null,
+          user: null
         }
-      }
+    }
+
+    componentDidMount(){
+      axios.get('https://young-shore-18643.herokuapp.com//songs')
+        .then(response => {
+          this.setState({
+           songs: response.data
+          });
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
+
+    onLoggedIn(user) {
+      this.setState({
+        user
+      });
+    }
 
   render() {
 
-    const {songs, selectedSong} = this.state;
+    const {songs, selectedSong, user} = this.state;
+    /* If there is no user, the LoginView is rendered. If there is a user logged in, the user details are *passed as a prop to the LoginView*/
+    if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
     if (selectedSong) return <SongView song={selectedSong} />;
     if (songs.length === 0){
         return <div className="main-view">The list is empty!</div>;
